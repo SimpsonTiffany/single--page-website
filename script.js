bf307f8a133358bf7ad56ab8d9ba599acd5a6bf9
SimpsonTiffany <simpsontiffany84@gmail.com>
2026-01-18 19:55:58 -0500

    Add event listeners for project button and contact form

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+// Event listener #1
+document.getElementById("projectBtn").addEventListener("click", function () {
+  document.getElementById("projectMessage").textContent =
+    "More projects coming soon!";
+});
+
+// Event listener #2
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById("nameInput").value;
+  document.getElementById("formResponse").textContent =
+    `Thank you for your message, ${name}!`;
+});
 
